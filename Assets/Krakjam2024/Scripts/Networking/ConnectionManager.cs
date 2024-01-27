11d48a26dcadcf19a637c5dba6be7f0c80d87712
Kamil Pastwa <kamil.pastwa@simfactor.pl>
2024-01-27 13:56:32 +0100

    init

---
+++ Assets/Krakjam2024/Scripts/Networking/ConnectionManager.cs
@@ -0,0 +1,181 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR.Client;
+using Placuszki.Krakjam2024.Server;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Placuszki.Krakjam2024
+{
+    public class ConnectionManager : MonoBehaviour, IGameHub
+    {
+        public event Action Connected;
+        public event Action Disconnected;
+
+        public UnityEvent ConnectingEvent;
+        public UnityEvent ConnectedEvent;
+        public UnityEvent DisconnectedEvent;
+
+        public bool IsConnected { get; private set; }
+
+        [SerializeField] private GameplayServiceConsumer _gameplayServiceConsumer;
+        private SignalRConnectionManager _connectionManager;
+        private IGameHub _client;
+        private Coroutine _startConnectionCoroutine;
+        private bool _stoppedManually;
+
+        void Awake()
+        {
+        }
+
+        void OnEnable()
+        {
+        }
+
+        void Start()
+        {
+            _client = this;
+            StartConnectionCoroutine();
+        }
+
+        void OnDisable()
+        {
+        }
+
+        void OnDestroy()
+        {
+            StopConnection();
+        }
+
+        public Task SendDataPacket(DataPacket dataPacket)
+        {
+            _gameplayServiceConsumer.ConsumeDataPacket(dataPacket);
+            return Task.CompletedTask;
+        }
+
+        private void StartConnectionCoroutine()
+        {
+            _startConnectionCoroutine = StartCoroutine(StartConnection());
+        }
+
+        private void RestartConnectionCoroutine()
+        {
+            if (_connectionManager != null)
+            {
+                StopConnection();
+            }
+            else if (_startConnectionCoroutine != null)
+            {
+                StopCoroutine(_startConnectionCoroutine);
+                _startConnectionCoroutine = null;
+            }
+
+            StartConnectionCoroutine();
+        }
+
+        private IEnumerator StartConnection()
+        {
+            yield return new WaitForEndOfFrame();
+
+            if (!IsConnected)
+            {
+                Init();
+                Connect();
+            }
+        }
+
+        private void StopConnection()
+        {
+            if (_startConnectionCoroutine != null)
+            {
+                StopCoroutine(_startConnectionCoroutine);
+            }
+
+            _stoppedManually = true;
+            _connectionManager.StopConnection();
+            Disconnected?.Invoke();
+            DisconnectedEvent.Invoke();
+            IsConnected = false;
+        }
+
+        private void Init()
+        {
+            var connectionString = GetConnectionString();
+            Debug.Log($"ConnectionString: {connectionString}");
+            try
+            {
+                _connectionManager = new SignalRConnectionManager(
+                    connectionString,
+                    TimeSpan.FromSeconds(ConnectionValues.TimeoutSeconds),
+                    TimeSpan.FromSeconds(ConnectionValues.KeepAliveSeconds),
+                    TimeSpan.FromSeconds(ConnectionValues.ReconnectTime));
+            }
+            catch (Exception e)
+            {
+                string msg = $"[SignalR] exception in SignalRConnectionManager cctor: {e.Message}";
+                Debug.LogError(msg);
+                StartConnectionCoroutine();
+                return;
+            }
+
+            _connectionManager.OnDisconnect += Disconnect;
+            _connectionManager.ConnectionCallback += OnConnected;
+            _connectionManager.Connection.On<DataPacket>(nameof(IGameHub.SendDataPacket), _client.SendDataPacket);
+            _connectionManager.Log += OnLog;
+        }
+
+        private void Disconnect()
+        {
+            UnityMainThreadDispatcher.Instance().EnqueueAsync(() => DisconnectedEvent.Invoke());
+
+            if (!_stoppedManually)
+            {
+                _connectionManager.Connection.DisposeAsync();
+                UnityMainThreadDispatcher.Instance().EnqueueAsync(() => _startConnectionCoroutine = StartCoroutine(StartConnection()));
+            }
+
+            Disconnected?.Invoke();
+            IsConnected = false;
+        }
+
+        private void Connect()
+        {
+            if (_connectionManager == null)
+            {
+                Debug.Log("_connectionManager is null, can't connect");
+                return;
+            }
+
+            ConnectingEvent.Invoke();
+            _stoppedManually = false;
+            _connectionManager.ConnectAsync();
+        }
+
+        private void OnConnected()
+        {
+            UnityMainThreadDispatcher.Instance().EnqueueAsync(() =>
+            {
+                Debug.Log("[SignalR] Connected to the server");
+                IsConnected = true;
+                Connected?.Invoke();
+                ConnectedEvent?.Invoke();
+            });
+        }
+
+        private void OnLog(string msg)
+        {
+            Debug.Log($"[{nameof(ConnectionManager)}] {msg}");
+        }
+
+        private string GetConnectionString()
+        {
+            string customServerIp = "217.182.74.11";
+            string port = "80";
+            string hub = "hubs/gamehub";
+
+            return $"http://{customServerIp}:{port}/{hub}";
+        }
+    }
+}
---
+++ Assets/Krakjam2024/Scripts/Networking/GameplayServiceConsumer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Placuszki.Krakjam2024.Server;
+using UnityEngine;
+
+namespace Placuszki.Krakjam2024
+{
+    public class GameplayServiceConsumer : MonoBehaviour
+    {
+        public event Action<DataPacket> OnDataPacketReceived;
+
+        public void ConsumeDataPacket(DataPacket dataPacket)
+        {
+            OnDataPacketReceived?.Invoke(dataPacket);
+        }
+    }
+}
