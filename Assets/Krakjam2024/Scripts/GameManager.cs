34741ec073c440dcedc04fc12c8a11a21ba96dd6
twinkeywinkey <twin98@poczta.fm>
2024-01-27 20:42:56 +0100

    Game loop? audio?

--- Assets/Krakjam2024/Scripts/GameManager.cs
+++ Assets/Krakjam2024/Scripts/GameManager.cs
@@ -1,5 +1,8 @@
+using Placuszki.Krakjam2024;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class GameManager : MonoBehaviour
 {
@@ -18,20 +21,49 @@ public class GameManager : MonoBehaviour
 
     #endregion
 
+    public event Action OnStartGame;
+    public event Action<string> OnEndGame;
+
     public GameObject _catprefab;
     public Transform[] _catSpawners;
     public int _catCount = 5;
+    public int _pointsToWin = 5;
 
-    private List<Cat> _activeCats;
+    [Space]
+    public AudioSource _gameMusic;
+    public AudioSource _menuMusic;
+
+    private List<Cat> _activeCats = new List<Cat>();
+    private Dictionary<string, int> _players = new Dictionary<string, int>();
+
+    private void Start()
+    {
+        _menuMusic?.Play();
+    }
+
+    public void RegisterPlayers(string playerID)
+    {
+        _players.Add(playerID, 0);
+    }
 
     public void CatHit(string playerID)
     {
-        CreateCat();
+        int playerPoints = _players[playerID];
+        _players[playerID] = playerPoints + 1;
+
+        if (!CheckIfSomeoneWin())
+        {
+            CreateCat();
+        }
+
     }
 
     [ContextMenu("Start")]
     public void StartGame()
     {
+        _gameMusic?.Play();
+        _menuMusic?.Stop();
+
         for (int i = 0; i < _catCount; i++)
         {
             CreateCat();
@@ -43,5 +75,34 @@ public class GameManager : MonoBehaviour
         var obj = Instantiate(_catprefab);
         obj.transform.position = _catSpawners[UnityEngine.Random.Range(0, _catSpawners.Length)].position;
         obj.GetComponent<Cat>().Init();
+    }
+
+    private void EndGame()
+    {
+        for (int i = 0; i < _activeCats.Count; i++)
+        {
+            _activeCats[i].DestroyCat();
+        }
+
+        _activeCats.Clear();
+        _players.Clear();
+
+        _gameMusic?.Stop();
+        _menuMusic?.Play();
+    }
+
+    private bool CheckIfSomeoneWin()
+    {
+        for (int i = 0; i < _players.Count; i++)
+        {
+            if (_players.ElementAt(i).Value >= _pointsToWin)
+            {
+                OnEndGame.Invoke(_players.ElementAt(i).Key);
+                EndGame();
+                return true;
+            }
+        }
+
+        return false;
     }
 }
