b9fa519f63696d6177f79d92463064b64ef9c4b9
Bartłomiej Siekacz <115244921+BartekSimfactor@users.noreply.github.com>
2024-01-27 19:23:22 +0100

    dupsko

--- Assets/Krakjam2024/Scripts/GameManager.cs
+++ Assets/Krakjam2024/Scripts/GameManager.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    #region Singleton
     public static GameManager Instance { get; private set; }
 
     private void Awake()
@@ -14,8 +15,25 @@ public class GameManager : MonoBehaviour
         Instance = this;
     }
 
+    #endregion
+
+    public GameObject _catprefab;
+    public Transform[] _catSpawners;
+    public int _catCount = 5;
+
     public void CatHit(int playerID)
     {
 
+    }
+
+    [ContextMenu("Start")]
+    public void StartGame()
+    {
+        for (int i = 0; i < _catCount; i++)
+        {
+            var obj = Instantiate(_catprefab);
+            obj.transform.position = _catSpawners[UnityEngine.Random.Range(0, _catSpawners.Length)].position;
+            obj.GetComponent<Cat>().Init();
+        }
     }
 }
