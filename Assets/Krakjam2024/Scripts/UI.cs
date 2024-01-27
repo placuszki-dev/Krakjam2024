17c345eb70aa198473bfeb3a07f432b156ec5429
Bartłomiej Siekacz <115244921+BartekSimfactor@users.noreply.github.com>
2024-01-27 21:51:38 +0100

    ui

--- Assets/Krakjam2024/Scripts/UI.cs
+++ Assets/Krakjam2024/Scripts/UI.cs
@@ -1,23 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
+using TMPro;
+using System;
+using static UnityEditor.Timeline.TimelinePlaybackControls;
 
 public class UI : MonoBehaviour
 {
-
     public Animation _uiAnim;
-   
-    IEnumerator Start()
-    {
-        _uiAnim.Play("IdleBack");
-        yield return new WaitForSeconds(1);
+    public TMP_Text MainText;
+    public TMP_Text WinText;
+    public CanvasGroup CanvasGroup;
+    public GameObject PanelWin;
+    public GameObject PanelMain;
 
+
+    void Start()
+    {
+        NewGame();
+        GameManager.Instance.OnEndGame += EndGame;
+        GameManager.Instance.OnStartGame += StartGame;
+    }
+
+    private void OnDestroy()
+    {
+        GameManager.Instance.OnEndGame -= EndGame;
+        GameManager.Instance.OnStartGame -= StartGame;
+    }
+
+    private void EndGame(string obj)
+    {
+        WinText.text = "Player " + obj + "Wins!";
+
+        CanvasGroup.DOFade(1, 2);
+        PanelWin.SetActive(true);
+        PanelMain.SetActive(false);
+    }
+
+    private void StartGame()
+    {
         _uiAnim.Play("ser");
+        CanvasGroup.DOFade(0, 3);
     }
 
-    // Update is called once per frame
-    void Update()
+    void NewGame()
     {
-        
+        PanelWin.SetActive(false);
+        PanelMain.SetActive(true);
+        _uiAnim.Play("serReverse");
+        MainText.DOFade(0, 3).From();
     }
+
 }
