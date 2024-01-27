57c8db83719f89c69fc5b99d303b072c7fc1c968
Pastew <kpastew@gmail.com>
2024-01-27 15:41:30 +0100

    feat: spawn player

--- Assets/Krakjam2024/Cannon/Scripts/Player.cs
+++ Assets/Krakjam2024/Cannon/Scripts/Player.cs
@@ -6,25 +6,7 @@ namespace Placuszki.Krakjam2024
     public class Player : MonoBehaviour
     {
         public string Id;
-
         [SerializeField] private MeshRenderer _inner;
-
-        public void SetColor(string hexColor)
-        {
-            Color newColor = HexToColor(hexColor);
-            _inner.material.color = newColor;
-        }
-
-        private Color HexToColor(string hex)
-        {
-            if (ColorUtility.TryParseHtmlString(hex, out var color))
-            {
-                return color;
-            }
-
-            Debug.LogError("Invalid hex color: " + hex);
-            return Color.white;
-        }
 
         public void HandleDataPacket(DataPacket dataPacket)
         {
@@ -34,6 +16,23 @@ namespace Placuszki.Krakjam2024
             Debug.Log(dataPacket.Y);
 
             SetColor(dataPacket.PhoneColor);
+        }
+
+        private void SetColor(string hexColor)
+        {
+            Color newColor = HexToColor(hexColor);
+            _inner.material.color = newColor;
+        }
+
+        private static Color HexToColor(string hex)
+        {
+            if (ColorUtility.TryParseHtmlString(hex, out var color))
+            {
+                return color;
+            }
+
+            Debug.LogError("Invalid hex color: " + hex);
+            return Color.white;
         }
     }
 }
