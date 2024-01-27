d3300c3ae2d7b5109fbf25a181b57c895ed0512c
twinkeywinkey <twin98@poczta.fm>
2024-01-27 17:43:28 +0100

    kotyAgent

--- Assets/Krakjam2024/Scripts/Cat.cs
+++ Assets/Krakjam2024/Scripts/Cat.cs
@@ -1,18 +1,56 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.AI.Navigation;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Cat : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public event Action OnHit;   
+
+
+    public NavMeshSurface surface;
+    NavMeshData data;
+    public NavMeshAgent agent;
+
+    public Animator animator;
+
+    float timer;
+
+    Vector3 destination;
+
+
     void Start()
     {
-        
+        data = surface.navMeshData;
+        agent.destination = SetRandomDest(data.sourceBounds);
+        timer = 0;
     }
 
-    // Update is called once per frame
+
+
+    //Update destination every 5 seconds to test
     void Update()
     {
-        
+        timer += Time.deltaTime;
+        if (timer > 5)
+        {
+            agent.destination = SetRandomDest(data.sourceBounds);
+            timer = 0;
+        }
+
+        animator.SetFloat("speed", agent.velocity.magnitude / agent.speed);
     }
+
+    Vector3 SetRandomDest(Bounds bounds)
+    {
+        var x = UnityEngine.Random.Range(bounds.min.x, bounds.max.x);
+        var z = UnityEngine.Random.Range(bounds.min.z, bounds.max.z);
+
+        destination = new Vector3(x, 1, z);
+        return destination;
+    }
+
 }
