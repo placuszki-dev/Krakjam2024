7c02f52ddb898f8a706961cb01508deb42d8aa31
Kamil Pastwa <kamil.pastwa@simfactor.pl>
2024-01-28 13:08:31 +0100

    create more players

--- Assets/Krakjam2024/Cannon/Scripts/DebugMenu.cs
+++ Assets/Krakjam2024/Cannon/Scripts/DebugMenu.cs
@@ -13,9 +13,17 @@ public class DebugMenu : MonoBehaviour
         {
             DebugRegisterPlayer();
         }
+        if (Input.GetKeyUp(KeyCode.T))
+        {
+            DebugRegisterPlayers();
+        }
         if (Input.GetKeyUp(KeyCode.S))
         {
             DebugShootRandomPlayer();
+        }
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            DebugShootRandomPlayers();
         }
         if (Input.GetKeyUp(KeyCode.C))
         {
@@ -42,15 +50,110 @@ public class DebugMenu : MonoBehaviour
         FindObjectOfType<DataPacketHandler>().HandleUserInfo(userInfo);
     }
     
-    public void DebugShootRandomPlayer()
+    public void DebugRegisterPlayers()
     {
-        var randomPlayer = FindObjectsOfType<Player>().ToList<Player>().GetRandomElement();
+        var dataPacketHandler = FindObjectOfType<DataPacketHandler>();
+        dataPacketHandler.HandleUserInfo(new UserInfo()
+        {
+            PlayerId = "DebugUser1",
+            PhoneColor = "#ff0000",
+            CheeseType = 1,
+        });
+        dataPacketHandler.HandleUserInfo(new UserInfo()
+        {
+            PlayerId = "DebugUser2",
+            PhoneColor = "#85a605",
+            CheeseType = 2,
+        });
+        dataPacketHandler.HandleUserInfo(new UserInfo()
+        {
+            PlayerId = "DebugUser3",
+            PhoneColor = "#696e98",
+            CheeseType = 1,
+        });
+        dataPacketHandler.HandleUserInfo(new UserInfo()
+        {
+            PlayerId = "DebugUser4",
+            PhoneColor = "#9860e5",
+            CheeseType = 1,
+        });
+        dataPacketHandler.HandleUserInfo(new UserInfo()
+        {
+            PlayerId = "DebugUser5",
+            PhoneColor = "#fd5fef",
+            CheeseType = 1,
+        });
+        dataPacketHandler.HandleUserInfo(new UserInfo()
+        {
+            PlayerId = "DebugUser6",
+            PhoneColor = "#54a1fd",
+            CheeseType = 2,
+        });
+        dataPacketHandler.HandleUserInfo(new UserInfo()
+        {
+            PlayerId = "DebugUser7",
+            PhoneColor = "#951d6d",
+            CheeseType = 2,
+        });
+        dataPacketHandler.HandleUserInfo(new UserInfo()
+        {
+            PlayerId = "DebugUser8",
+            PhoneColor = "#089f3e",
+            CheeseType = 2,
+        });
+        dataPacketHandler.HandleUserInfo(new UserInfo()
+        {
+            PlayerId = "DebugUser9",
+            PhoneColor = "#cfca3d",
+            CheeseType = 1,
+        });
+        dataPacketHandler.HandleUserInfo(new UserInfo()
+        {
+            PlayerId = "DebugUser10",
+            PhoneColor = "#57e047",
+            CheeseType = 2,
+        });
+        dataPacketHandler.HandleUserInfo(new UserInfo()
+        {
+            PlayerId = "DebugUser11",
+            PhoneColor = "#e17d79",
+            CheeseType = 1,
+        });
+        dataPacketHandler.HandleUserInfo(new UserInfo()
+        {
+            PlayerId = "DebugUser12",
+            PhoneColor = "#99627a",
+            CheeseType = 2,
+        });
+    }
+
+    private void DebugShootRandomPlayer()
+    {
         DataPacket dp = new DataPacket()
         {
-            PlayerId = "DebugUser",
+            PlayerId = "DebugUser1",
             X = Random.Range(-0.5f, 0.5f),
             Y = Random.Range(0.5f, 1),
         };
         FindObjectOfType<DataPacketHandler>().HandleDataPacket(dp);
+    }
+
+    private void DebugShootRandomPlayers()
+    {
+        DataPacketHandler dataPacketHandler = FindObjectOfType<DataPacketHandler>();
+
+        for (int i = 1; i <= 12; i++)
+        {
+            if(Random.Range(0, 100) > 70f)
+                continue;
+            
+            DataPacket dp = new DataPacket()
+            {
+                PlayerId = $"DebugUser{i}",
+                X = Random.Range(-0.5f, 0.5f),
+                Y = Random.Range(0.5f, 1),
+            };
+            dataPacketHandler.HandleDataPacket(dp);
+        }
     }
 }
