6679112437fe3a74b81b8c21dda77031ddceadd1
Pastew <kpastew@gmail.com>
2024-02-06 21:08:40 +0100

    fix: auto connect, disable old canvas, add more info to main menu, add qr code

--- Assets/Scripts/JoinServer.cs
+++ Assets/Scripts/JoinServer.cs
@@ -1,31 +1,13 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using TMPro;
-using UnityEditor;
 using UnityEngine;
-using UnityEngine.UI;
 
 namespace WebglExample
 {
     public class JoinServer : MonoBehaviour
     {
-        public Button Connect;
-        public TMP_InputField Name;
-        public TMP_InputField GameId;
         public SignalrConnection Connection;
         void Start()
         {
-            Name.text = Guid.NewGuid().ToString();
-            Connection.OnStarted.AddListener((id) =>
-            {
-                Connection.JoinGame(Name.text, GameId.text);
-                gameObject.SetActive(false);
-            });
-            Connect.onClick.AddListener(() =>
-            {
-                Connection.StartConnection();
-            });
+            Connection.StartConnection();
         }
     }
 }
